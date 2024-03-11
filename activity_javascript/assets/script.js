1e54da906c94a1aa7a6467b277c895eaefa11810
CSSPracticer25 <dejucjo@gmail.com>
2024-03-12 05:23:13 +0800

    activity_javascript

--- activity_javascript/assets/script.js
+++ activity_javascript/assets/script.js
@@ -1,17 +1,18 @@
 let Execute = document.getElementById("btnExecute");
 
 Execute.addEventListener('click', function() {
-    let Limit = document.getElementById("txtLimitRow").value;
-    let Limit2 = document.getElementById("txtLimitColumn").value;
-    let Divs = "";
-    for(var x = 1; x <= Limit; x++) {
-       Divs += "";
-        for(var y = 1; y <= Limit2; y++) {
-            Divs += "<div class='text-center bg-secondary m-2 p-2' style='width: 40px; height: 40px;'>" + (x * y) + "</div>";
-            Divs += "";
+    let LimitRow = document.getElementById("txtLimitRow").value;
+    let LimitColumn = document.getElementById("txtLimitColumn").value;
+
+    let Divs = "<table class='table table-bordered'>";
+    for (let x = 1; x <= LimitRow; x++) {
+        Divs += "<tr>";
+        for (let y = 1; y <= LimitColumn; y++) {
+            Divs += "<td class='text-center text-white bg-secondary m-2 p-2' style='width: 40px; height: 40px;'>" + (x * y) + "</td>";
         }
-       
+        Divs += "</tr>";
     }
+    Divs += "</table>";
 
     document.getElementById("div-wrapper").innerHTML = Divs;
 });
